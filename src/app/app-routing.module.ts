89e9e204cc1e76cd9f7fb5578fde64fa37fef9df
mahdi fajraoui <mahdifajraoui@gmail.com>
2018-03-13 23:00:43 +0100

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { FirstComponent } from './first/first.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OtherComponent } from './other/other.component';
+import { componentFactoryName } from '@angular/compiler';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',component:OtherComponent},
+  {path:'other',component:OtherComponent},
+  {path:'first',component:FirstComponent},
+  {path:"*",component:OtherComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routes } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
 import { LoginService } from './services/login.service';
+import { OtherComponent } from './other/other.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent
+    FirstComponent,
+    OtherComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [LoginService
   ],
