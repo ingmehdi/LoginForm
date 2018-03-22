206e1e0b39876b4e3d1eb19c081866085abac8bb
mahdi fajraoui <mahdifajraoui@gmail.com>
2018-03-22 22:42:47 +0100

    fix code

--- src/app/services/callapi.service.ts
+++ src/app/services/callapi.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Http} from '@angular/http'
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class CallapiService {
@@ -8,7 +9,7 @@ export class CallapiService {
 //call api
 getPosts(){
 
-  return this.http.get('https://jsonplaceholder.typicode.com/posts');
+  return this.http.get('https://jsonplaceholder.typicode.com/posts').map(result=>result.json());
 
 }
 }
